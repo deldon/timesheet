4f00dd1ada96b794f57241f256efdd6addd6159e
deldon <r.deldon@gmail.com>
2024-08-30 07:46:30 +0200

    build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false, // Désactiver les sourcemaps en production
+    outDir: 'dist',  // Répertoire de sortie pour les fichiers de build
+    sourcemap: false, // Désactiver les sourcemaps en production pour économiser de l'espace
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   },
+  server: {
+    port: 3000,  // Port pour le serveur de développement local
+  },
+  base: '/', // Assurez-vous que cela correspond à la racine de votre URL sur le serveur Caddy
+
 })
